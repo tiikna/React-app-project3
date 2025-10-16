ce73ae6f1a448cfb24e601369947154840c124a4
AK <149267174+tiikna@users.noreply.github.com>
2025-10-16 23:26:50 +0530

    Set Docker image based on branch name
    Add conditional logic to set Docker image based on branch.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
+
+# Default to dev
+BRANCH_NAME="${BRANCH_NAME:-dev}"
+
+if [ "$BRANCH_NAME" = "master" ]; then
+  export DOCKER_IMAGE="mishraankit062/reactapp3-prod:latest"
+else
+  export DOCKER_IMAGE="mishraankit062/reactapp3-dev:latest"
+fi
+
 docker-compose down
 docker-compose up -d --build
